0e1266331129a297fa7530014f4ce9592e18bdda
Ben Taylor <me@taybenlor.com>
2012-08-10 00:00:58 +1000

    Use jerserderds typekit

--- app.rb
+++ app.rb
@@ -13,6 +13,12 @@ get '*' do
     doc.traverse do |node|
       if node.text?
         node.content = Ermahgerd.ternslert_terkst node.content
+      elsif node.name == 'script'
+        if node.has_attribute?('src') && node.get_attribute('src').match(/use\.typekit\.net/)
+          node.set_attribute 'src', "//use.typekit.net/eed3xqc.js"
+        end
+        # jessedodds.com <script type="text/javascript" src="//use.typekit.net/nip2dbc.js"></script>
+        # jerserderds.com <script type="text/javascript" src="//use.typekit.net/eed3xqc.js"></script>
       end
     end
     body = doc.to_html
